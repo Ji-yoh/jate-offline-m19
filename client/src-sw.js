56f889f2e2cca0d0442d7d594dd0ecec13de9632
Jiyoh Macbook <geovannih514@gmail.com>
2023-09-27 21:04:09 -0700

    installed missing dependencies, need to work on db entries

--- client/src-sw.js
+++ client/src-sw.js
@@ -28,9 +28,7 @@ registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
 // TODO: Implement asset caching
 registerRoute(
-  ({request}) => request.destination === 'script' ||
-  request.destination === 'style' ||
-  request.destination === 'font',
+  ({request}) => ['style', 'script', 'worker'].includes(request.destination),
   new CacheFirst({ // this creates the cache
     cacheName: 'assets-cache',
     plugins: [
@@ -45,8 +43,3 @@ registerRoute(
   })
 );
 
-offlineFallback({
-  pageFallback: '/index.html',
-  imageFallback: null,
-  debug: true,
-})
